a17bdb5e45c7c8ce8db1aa95eab32fbb8b72458c
AnonymoxB <Anonymoxb12@gmail.com>
2025-08-18 17:54:33 +0700

    y

--- pos-backend/app.js
+++ pos-backend/app.js
@@ -18,7 +18,7 @@
 
     app.use(cors({
         credentials: true,
-        origin: ['http://localhost:5173', 'https://pos-wine-two.vercel.app/']
+        origin: ['http://localhost:5173', 'https://pos-wine-two.vercel.app']
     }))
 
     app.use(express.json());
