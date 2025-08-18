def508de1b35481472166328c2069f538f95a4a1
AnonymoxB <Anonymoxb12@gmail.com>
2025-08-18 17:36:10 +0700

    y

--- pos-backend/app.js
+++ pos-backend/app.js
@@ -18,7 +18,7 @@
 
     app.use(cors({
         credentials: true,
-        origin: ['http://localhost:5173']
+        origin: ['http://localhost:5173', 'https://pos-frontend.vercel.app']
     }))
 
     app.use(express.json());
