299ece7d5f495c884a64c8e2a54b8ae9e8df6d41
bgsnnd <bgsnnd8@gmail.com>
2025-08-20 10:04:57 +0700

    ascasc

--- pos-frontend/src/components/orders/OrderCard.jsx
+++ pos-frontend/src/components/orders/OrderCard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { FaCheckDouble, FaCircle } from "react-icons/fa";
-import { getOrders } from "../../api/axiosWrapper";
+import { getOrders } from "../../https";
 
 const OrderCard = ({ order }) => {
   if (!order) return null;
